8e52a5611b6775980a698800b84af76eece486da
hoapha1009 <hoapha1009@gmail.com>
2021-04-07 20:00:15 +0700

    Feat: NotFound component, TodoList features

---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.scss";
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <div className="err">4</div>
+            <i className="far fa-question-circle fa-spin"></i>
+            <div className="err2">4</div>
+            <div className="msg">
+                Maybe this page moved? Got deleted? Is hiding out in quarantine?
+                Never existed in the first place?
+                <p>
+                    Let's go <a href="/">home</a> and try from there.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
--- src/features/Todo/index.tsx
+++ src/features/Todo/index.tsx
@@ -1,8 +1,115 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import TodoList from "./components/TodoList";
 import "./styles.scss";
+import queryString from "query-string";
+import { useHistory, useLocation, useRouteMatch } from "react-router";
+
+interface TodoItem {
+    id: number;
+    title: string;
+    status: string;
+}
 
 const TodoFeature = () => {
-    return <div></div>;
+    const location = useLocation();
+    const history = useHistory();
+    const match = useRouteMatch();
+
+    const [filtered, setFiltered] = useState(() => {
+        const params = queryString.parse(location.search);
+
+        return params.status || "all";
+    });
+
+    const [todoList, setTodoList] = useState<TodoItem[]>((): TodoItem[] => {
+        return [
+            {
+                id: 1,
+                title: "Eat",
+                status: "new",
+            },
+            {
+                id: 2,
+                title: "Sleep",
+                status: "completed",
+            },
+            {
+                id: 3,
+                title: "Code",
+                status: "new",
+            },
+        ];
+    });
+
+    const handleTodoClick = (todo: TodoItem, idx: number) => {
+        const newTodoList = [...todoList];
+
+        const newTodo = {
+            ...newTodoList[idx],
+            status:
+                newTodoList[idx].status === "completed" ? "new" : "completed",
+        };
+
+        newTodoList[idx] = newTodo;
+
+        setTodoList(newTodoList);
+    };
+
+    const handleShowAllClick = () => {
+        const queryParams = {
+            status: "all",
+        };
+        history.push({
+            pathname: match.path,
+            search: queryString.stringify(queryParams),
+        });
+    };
+
+    const handleShowCompletedClick = () => {
+        const queryParams = {
+            status: "completed",
+        };
+        history.push({
+            pathname: match.path,
+            search: queryString.stringify(queryParams),
+        });
+    };
+
+    const handleShowNewClick = () => {
+        const queryParams = {
+            status: "new",
+        };
+        history.push({
+            pathname: match.path,
+            search: queryString.stringify(queryParams),
+        });
+    };
+
+    useEffect(() => {
+        const params = queryString.parse(location.search);
+        setFiltered(params.status || "all");
+    }, [location.search]);
+
+    const renderTodoList = useMemo(() => {
+        return todoList.filter(
+            (todo) => todo.status === filtered || filtered === "all"
+        );
+    }, [todoList, filtered]);
+
+    return (
+        <div>
+            <h3>Todo List</h3>
+            <TodoList todoList={renderTodoList} onTodoClick={handleTodoClick} />
+
+            <div>
+                <button onClick={handleShowAllClick}>Show All</button>
+                <button onClick={handleShowCompletedClick}>
+                    Show Completed
+                </button>
+                <button onClick={handleShowNewClick}>Show New</button>
+            </div>
+        </div>
+    );
 };
 
 export default TodoFeature;
