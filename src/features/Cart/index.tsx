38269214fca25e129e3a5ef7f9dab455e4ab64dc
hoapha1009 <hoapha1009@gmail.com>
2021-04-28 10:57:42 +0700

    Feat: Add checkout button and Edit checkout page

--- src/features/Cart/index.tsx
+++ src/features/Cart/index.tsx
@@ -2,12 +2,14 @@ import {
     Box,
     Button,
     Container,
+    Grid,
     makeStyles,
     Paper,
     Theme,
     Typography,
 } from '@material-ui/core';
 import React from 'react';
+import { useHistory } from 'react-router';
 import { useAppSelector } from '../../app/hooks';
 import { formatPrice } from '../../utils';
 import CartItem from './cartItem';
@@ -26,6 +28,7 @@ const useStyles = makeStyles((theme: Theme) => ({
         display: 'flex',
         flexFlow: 'row nowrap',
         justifyContent: 'space-between',
+        alignItems: 'baseline',
         padding: theme.spacing(2),
     },
     text: {
@@ -42,10 +45,14 @@ const useStyles = makeStyles((theme: Theme) => ({
         fontSize: '24px',
         color: theme.palette.secondary.main,
     },
+    button: {
+        marginLeft: theme.spacing(2),
+    },
 }));
 
 const CartFeature = () => {
     const classes = useStyles();
+    const history = useHistory();
     const cartItems = useAppSelector((state) => state.cart.cartItems);
     const cartItemsCount = useAppSelector(cartItemsCountSelector);
     const cartItemsTotal = useAppSelector(cartItemsTotalSelector);
@@ -64,12 +71,63 @@ const CartFeature = () => {
                         <Box component='span' className={classes.price}>
                             {formatPrice(cartItemsTotal)}
                         </Box>
+                        <Box component='span' className={classes.button}>
+                            <Button
+                                variant='contained'
+                                color='secondary'
+                                size='large'
+                                // onClick={}
+                            >
+                                Thanh toán
+                            </Button>
+                        </Box>
                     </Typography>
                 </Box>
-                {cartItemsCount > 0 ? (
+
+                <Grid container spacing={1}>
+                    {cartItemsCount > 0 ? (
+                        <>
+                            <Grid item xs={12}>
+                                {cartItems.map((product: any) => (
+                                    <CartItem
+                                        key={product.id}
+                                        product={product}
+                                    />
+                                ))}
+                            </Grid>
+                        </>
+                    ) : (
+                        <Grid item xs={12}>
+                            <Paper className={classes.root} elevation={0}>
+                                <img
+                                    src='https://salt.tikicdn.com/desktop/img/mascot@2x.png'
+                                    alt='empty-cart'
+                                    width='190px'
+                                />
+                                <Typography
+                                    variant='body1'
+                                    className={classes.text}
+                                >
+                                    Không có sản phẩm nào trong giỏ hàng của bạn
+                                </Typography>
+                                <Button
+                                    variant='contained'
+                                    color='secondary'
+                                    size='large'
+                                    onClick={() => history.push('/')}
+                                >
+                                    Tiếp tục mua sắm
+                                </Button>
+                            </Paper>
+                        </Grid>
+                    )}
+                </Grid>
+
+                {/* {cartItemsCount > 0 ? (
                     cartItems.map((product: any) => (
                         <CartItem key={product.id} product={product} />
                     ))
+                    
                 ) : (
                     <Paper className={classes.root}>
                         <img
@@ -84,11 +142,12 @@ const CartFeature = () => {
                             variant='contained'
                             color='secondary'
                             size='large'
+                            onClick={() => history.push('/')}
                         >
                             Tiếp tục mua sắm
                         </Button>
                     </Paper>
-                )}
+                )} */}
             </Container>
         </Box>
     );
